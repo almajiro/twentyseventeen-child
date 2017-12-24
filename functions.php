43b7e2a6aedb079cadfa2a415b6c3aabe960d63d
Kuroki Almajiro <kuroki@almajiro.tokyo>
2017-12-24 19:25:09 +0900

    steins;gateのウィジェットを追加

--- functions.php
+++ functions.php
@@ -1,7 +1,19 @@
 <?php
 
+require_once locate_template('widgets.php', true);
+
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+add_action('admin_bar_menu', 'admin_bar_custom', 11);
 add_filter('admin_footer_text', 'remove_footer_admin');
+
+function admin_bar_custom($wp_admin_bar)
+{
+	$wp_admin_bar->remove_menu('wp-logo');
+	$wp_admin_bar->add_menu(array(
+		'id' => 'customized-by',
+		'title' => 'WordPress - customized by almajiro'
+	));
+}
 
 function remove_footer_admin()
 {
