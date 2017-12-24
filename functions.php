a33a2e9f2cce64bbbaaa6487f1845581ced7b5cf
Kuroki Almajiro <kuroki@almajiro.tokyo>
2017-12-24 18:04:49 +0900

    functions.phpへremove_footer_admin()の追加

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+add_filter('admin_footer_text', 'remove_footer_admin');
+
+function remove_footer_admin()
+{
+	echo '<span id="footer-thankyou">&copy; almajiro All rights reserved.</span>';
+}
+
 function theme_enqueue_styles()
 {
         wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
